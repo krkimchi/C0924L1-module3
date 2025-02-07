9602af38b8a8e5573799000e6ecdc82a90cffc77
Lê Anh Quân <anhquan.dns@gmail.com>
2025-02-07 07:24:25 -0600

    ss1

---
+++ database/ss1_tong_quan_csdl.sql
@@ -0,0 +1,14 @@
+create database if not exists student_management;
+use student_management;
+
+create table class (
+	id int primary key auto_increment,
+    name varchar(100) not null
+);
+
+create table teacher (
+     id int primary key auto_increment,
+     name varchar(100) not null,
+     age int not null check(age > 0),
+     country varchar(100) not null
+);
